ee390ad6eff91f9be44cc50075cd59627a078057
chongfengwudi <52685084+chfwd@users.noreply.github.com>
2023-01-06 21:41:00 +0800

    Update info.go
    Hopefully I'll see a FastBuilder update when I wake up tomorrow.

--- minecraft/protocol/info.go
+++ minecraft/protocol/info.go
@@ -2,8 +2,8 @@ package protocol
 
 const (
 	// CurrentProtocol is the current protocol version for the version below.
-	CurrentProtocol = 475
+	CurrentProtocol = 503
 	// CurrentVersion is the current version of Minecraft as supported by the `packet` package.
-	CurrentVersion = "1.18.10"
-	// 475 -> 1.18.0.
+	CurrentVersion = "1.18.30"
+	// 503 -> 1.18.30.
 )
